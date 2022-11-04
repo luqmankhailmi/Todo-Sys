c7b53da67e6a2624cf68703bd7ed9167c177ba47
Luqman:D <111408610+Soulnim@users.noreply.github.com>
2022-11-04 18:09:14 +0800

    Update todo.cpp

--- Main/todo.cpp
+++ Main/todo.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
 using namespace std;
 
+void menu() {
+	cout << "---------------------------------------" << endl;
+	cout << " To Do List" << endl;
+	cout << " 1. View List" << endl;
+	cout << " 2. Add To List" << endl;
+	cout << " 3. Delete From List" << endl;
+	cout << "---------------------------------------" << endl;
+}
+
 int main() {
-  cout << "nothing here" << endl;
-  cout << "test" << endl;
-  return 0;
+	string todoList[100];
+	int key;
+	
+	bool start = true;
+	while (start == true) {
+		
+		menu();
+		cout << "Choose key : ";
+		cin >> key;
+	
+		if (key == 1) {
+			int iterator = 0;
+			bool continuePrint = true;
+			cout << "--------------------------------" << endl;
+			cout << "View List" << endl;
+			cout << "--------------------------------" << endl;
+			
+			while (continuePrint == true) {
+				if (todoList[iterator].length() != 0) {
+					cout << iterator + 1 << ". " << todoList[iterator] << endl;
+					iterator++;
+				} else {
+					continuePrint = false;
+				}
+			}
+			
+			char quitKey;
+			bool quitView = false;
+			while (quitView == false) {
+				cout << "Press \'q\' to exit : ";
+				cin >> quitKey;
+				
+				if (quitKey == 'q') {
+					quitView = true;
+				}
+			}
+		}
+	}
 }
